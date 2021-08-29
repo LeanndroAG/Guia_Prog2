f5b04b3adf8e647080fef7a0edf2c3802e83f997
Leandro AG <leanndro.ps4@gmail.com>
2021-08-29 10:45:07 -0300

    Ejercicio10

---
+++ Ejercicio_10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ejercicio_10
+{
+    /*
+        Partiendo de la base del ejercicio anterior, se pide realizar un programa que imprima por pantalla
+        una pirámide como la siguiente:
+                                            *
+                                           ***
+                                          *****
+                                         *******
+                                        *********
+        Nota: Utilizar estructuras repetitivas y selectivas. 
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int alto;
+            Console.WriteLine("Alto: ");
+            alto = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < alto; i++)
+            {
+                Console.WriteLine(new String(' ', alto - i - 1) + new String('*', i + i + 1));
+            }
+        }
+    }
+}
