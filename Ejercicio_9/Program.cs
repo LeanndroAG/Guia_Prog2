49f2444e43b55ca421b3f990aa72a6d32adf03d4
Leandro AG <leanndro.ps4@gmail.com>
2021-08-29 10:37:16 -0300

    Ejercicio9

---
+++ Ejercicio_9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ejercicio_9
+{
+    /*
+        Escribir un programa que imprima por pantalla una pirámide como
+        la siguiente:
+        *
+        ***
+        *****
+        *******
+        *********
+        El usuario indicará cuál será la altura de la pirámide ingresando un número entero positivo. Para el
+        ejemplo anterior la altura ingresada fue de 5.
+        Nota: Utilizar estructuras repetitivas y selectivas. 
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alto;
+            Console.WriteLine("Alto: ");
+            alto = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < alto; i++)
+            {
+                for (int k = 0; k < alto; k++)
+                {
+                    if (k <= i)
+                    {
+                        Console.Write("*");
+                    }
+                    if (k == i)
+                    {
+                        Console.Write("\n");
+                    }
+                }
+            }
+
+        }
+    }
+}
