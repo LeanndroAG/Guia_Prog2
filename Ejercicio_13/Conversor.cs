054cbc5c74be25018c4c8869851c51bea7e5e4d9
Leandro AG <leanndro.ps4@gmail.com>
2021-08-30 10:17:08 -0300

    Ejercicio13

---
+++ Ejercicio_13/Conversor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+       Desarrollar una clase llamada Conversor, que posea dos métodos de clase (estáticos):
+       string DecimalBinario(int). Convierte un número de entero a binario.
+       int BinarioDecimal(string). Convierte un número binario a entero.
+       NOTA: no utilizar los atajos del lenguaje, hacerlo mediante estructuras repetitivas y de control.
+*/
+namespace Ejercicio_13
+{
+    public class Conversor
+    {
+        public static string DecimalBinario(int numero)
+        {
+            string binario = "";
+
+            while (numero > 0)
+            {
+                binario = numero % 2 + binario;
+                numero = numero / 2;
+            }
+
+            return binario;
+        }
+        public static int BinarioDecimal(string binario)
+        {
+             int exponente = binario.Length - 1;
+             int numeroDecimal = 0;
+
+             for (int i = 0; i < binario.Length; i++)
+             {
+                 if (int.Parse(binario.Substring(i, 1)) == 1)
+                 {
+                     numeroDecimal = numeroDecimal + Convert.ToInt32(Math.Pow(2, Convert.ToDouble(exponente.ToString())).ToString());
+                 }
+                 exponente--;
+             }
+             return numeroDecimal;
+        }
+    }
+}
---
+++ Ejercicio_13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ejercicio_13
+{
+    /*
+        Desarrollar una clase llamada Conversor, que posea dos métodos de clase (estáticos):
+        string DecimalBinario(int). Convierte un número de entero a binario.
+        int BinarioDecimal(string). Convierte un número binario a entero.
+        NOTA: no utilizar los atajos del lenguaje, hacerlo mediante estructuras repetitivas y de control.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string binario;
+
+            Console.Write("Decimal a Binario: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            binario = Conversor.DecimalBinario(numero);
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Binario: {0}", binario);
+            Console.WriteLine("------------------------------------------------------------");
+            numero = 0;
+            binario = "";
+          
+            Console.Write("Binario a Decimal: ");
+            binario = Console.ReadLine();
+            numero = Conversor.BinarioDecimal(binario);
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Decimal: {0}", numero);
+            Console.WriteLine("------------------------------------------------------------");
+
+
+        }
+    }
+}
