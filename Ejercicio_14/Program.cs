4113d86129a79fb57b1323f63a34f756f39f5523
Leandro AG <leanndro.ps4@gmail.com>
2021-08-31 05:43:51 -0300

    Ejercicios14-15

---
+++ Ejercicio_14/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ejercicio_14
+{
+    /*
+     *  Realizar una clase llamada CalculoDeArea que posea 3 métodos de clase (estáticos) que realicen el
+        cálculo del área que corresponda:
+        a. double CalcularCuadrado(double)
+        b. double CalcularTriangulo(double, double)
+        c. double CalcularCirculo(double)
+        El ingreso de los datos como la visualización se deberán realizar desde el método Main().
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lado,laBase,laAltura,radio;
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Area de un Cuadrado");
+            Console.Write("Ingresar lado: ");
+            lado = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Area: {0}\n", CalculoDeArea.CalcularCuadrado(lado));
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Area de un Triangulo");
+            Console.Write("Ingresar base: ");
+            laBase = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingresar Altura: ");
+            laAltura = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Area: {0}\n", CalculoDeArea.CalcularTriangulo(laBase,laAltura));
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Area de un Circulo");
+            Console.Write("Ingresar radio: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Area: {0}\n", CalculoDeArea.CalcularCirculo(radio));
+
+
+        }
+    }
+}
---
+++ Ejercicio_15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_15
+{
+    class Program
+    {
+        /*
+            Realizar un programa que permita realizar operaciones matemáticas simples (suma, resta,
+            multiplicación y división). Para ello se le debe pedir al usuario que ingrese dos números y la
+            operación que desea realizar (pulsando el caracter +, -, * o /).
+            El usuario decidirá cuándo finalizar el programa.
+            Crear una clase llamada Calculadora que posea dos métodos estáticos (de clase):
+            a. Calcular (público): Recibirá tres parámetros, el primer número, el segundo número y la
+            operación matemática. El método devolverá el resultado de la operación.
+            b. Validar (privado): Recibirá como parámetro el segundo número. Este método se debe
+            utilizar sólo cuando la operación elegida sea la DIVISIÓN. Este método devolverá
+            TRUE si el número es distinto de CERO. 
+         */
+        static void Main(string[] args)
+        {
+            int operandoA, operandoB;
+            char operador;
+            Console.WriteLine("CALCULADORA");
+            Console.Write("Ingrese 1er operando: ");
+            operandoA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese 2do operando: ");
+            operandoB = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese operador + - * / : ");
+            operador =Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Resultado: {0}", Calculadora.Calcular(operandoA, operandoB, operador));
+        }
+    }
+}
