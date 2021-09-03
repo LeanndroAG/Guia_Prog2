9e00b8c20900d4fec763c9d8a25e15ab31306e7f
Leandro AG <leanndro.ps4@gmail.com>
2021-09-02 21:42:04 -0300

    Ejercicio16

---
+++ Ejercicio_16/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
